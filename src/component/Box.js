36ebf6a0f1eab1c7197da7c1a13a5e66e374af9a
HCJIN <ghkdckswls02@naver.com>
2023-04-18 09:56:24 +0900

    first commit

---
+++ src/component/Box.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Box = () => {
+  return (
+    <div className='box'>
+      <h1>YOU</h1>
+      <img className='item-img' src='https://blog.kakaocdn.net/dn/HfURw/btqXKvOTNWK/gWTwPXEg9QzSV0ilOuwuak/img.png' alt=''/>
+      <h2>WIN</h2>
+    </div>
+  )
+}
+
+export default Box
